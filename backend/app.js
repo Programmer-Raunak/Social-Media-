c87f03a6cac36c245c8650b8c24ec7099e93bb3c
𝐑𝐀𝐔𝐍𝐀𝐊  ||  ᴡᴇʙ ᴅᴇᴠᴇʟᴏᴘᴇʀ <158747587+Programmer-Raunak@users.noreply.github.com>
2024-11-09 03:30:45 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,14 +15,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
-
+app.use(cors({
+  origin:[],
+  method:["POST","GET"],
+  credentials:true
+}));
+app.use(express.json())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
