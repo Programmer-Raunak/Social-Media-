59a3875c820510b417ba22733c7f55f6a87b5b30
𝐑𝐀𝐔𝐍𝐀𝐊  ||  ᴡᴇʙ ᴅᴇᴠᴇʟᴏᴘᴇʀ <158747587+Programmer-Raunak@users.noreply.github.com>
2024-11-10 04:09:26 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,11 +20,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({
-  origin:["https://social-media-seven-amber.vercel.app/"],
-  method:["POST","GET"],
-  credentials:true
-}));
+app.use(cors());
 app.use(express.json())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
